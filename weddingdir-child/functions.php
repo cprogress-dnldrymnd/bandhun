24df3c131ef61c926eaa0b8e669401658534ca92
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-14 19:58:34 +0800

    css

--- weddingdir-child/functions.php
+++ weddingdir-child/functions.php
@@ -189,7 +189,6 @@ function action_wp_footer()
             $filter_backdrop.appendTo('body');
             $map_button = jQuery('<li class="nav-item nav-map"> <a class="nav-link" id="listing-map" data-bs-toggle="pill" href="#" role="tab" aria-selected="false"> <i class="fa fa-map-marker"></i> Map </a> </li>');
             $map_button.appendTo('.map-tabbing');
-
             $map_close = jQuery('<svg class="close-map" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path> </svg>');
 
             $map_close.insertAfter('.map-container');
